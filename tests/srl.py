e36b9dfd6cedea9099dab8b7212feb96214e2845
Alexandre Hassan <alexandre.hassan@nokia.com>
2022-08-19 11:28:22 -0500

    push tests to not have to make

---
+++ tests/srl.py
@@ -0,0 +1,33 @@
+from pygnmi.client import gNMIclient
+
+host = ("clab-support-dev-srl1", 57400)
+cert = "/ca/srl1/srl1.pem"
+enc = "json_ietf"
+
+
+class srl:
+    def __init__(self):
+        self.gc = gNMIclient(
+            target=host,
+            username="admin",
+            password="admin",
+            insecure=False,
+            debug=False,
+            path_cert=cert,
+        )
+        self.gc.__enter__()
+
+    def start_agent(self):
+        m = [("/tools/system/app-management/application[name=support]", {"start": ""})]
+        self.gc.set(update=m, encoding=enc)
+
+    def stop_agent(self):
+        m = [("/tools/system/app-management/application[name=support]", {"stop": ""})]
+        self.gc.set(update=m, encoding=enc)
+
+    def get_agent_status(self):
+        result = self.gc.get(
+            path=["/system/app-management/application[name=support]/state"],
+            encoding=enc,
+        )
+        return result["notification"][0]["update"][0]["val"]
